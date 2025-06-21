fef1ed7ceeea6fc8daaae3662ded9977c1a65c33
Eric <eric_sentient@berkeley.edu>
2025-06-20 20:28:30 -0400

    added skill cost because i forgor

--- scenes/combat/skills/skills_list/fire_t1.gd
+++ scenes/combat/skills/skills_list/fire_t1.gd
@@ -4,15 +4,19 @@ var spell_fx_basic = preload("res://scenes/combat/animation/spell_effect_basic.t
 
 func play_action(details : Dictionary) -> void:
 	var entity : CombatEntity = details.targetEntity
-	#TODO: replace this copy pasted code you dung beetle
-	var spell : AnimatedSprite3D = spell_fx_basic.instantiate()
-	var callback_free = func():
-		apply_effect(details)
-		spell.queue_free()		
-	spell.play('Fire')
-	spell.animation_finished.connect(callback_free)
-	entity.add_child(spell)
-
+	if (details.user.apply_skill_cost(self)):
+		var spell : AnimatedSprite3D = spell_fx_basic.instantiate()
+		var callback_free = func():
+			apply_effect(details)
+			spell.queue_free()
+		spell.play('Fire')
+		spell.animation_finished.connect(callback_free)
+		entity.add_child(spell)
+	else:
+		print('skill cost could not be applied!')
+		var callback : Callable = details.callback
+		callback.call()
+		
 func apply_effect(details: Dictionary) -> void:
 	var callback : Callable = details.callback
 	var entity : CombatEntity = details.entity
